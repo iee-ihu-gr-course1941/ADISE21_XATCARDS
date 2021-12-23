ee9b923a962475da519e884960212e16642dc5c2
tmitsis <tmitsis@hotmail.gr>
2021-12-23 13:05:51 +0200

    temporary schema

---
+++ Mountzouris/schema.sql
@@ -0,0 +1,226 @@
+-- MariaDB dump 10.19  Distrib 10.4.22-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: mountzouris
+-- ------------------------------------------------------
+-- Server version	10.4.22-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `board`
+--
+
+DROP TABLE IF EXISTS `board`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `board` (
+  `x` tinyint(1) NOT NULL,
+  `y` tinyint(1) NOT NULL,
+  `b_color` enum('B','W') NOT NULL,
+  `piece_color` enum('W','B') DEFAULT NULL,
+  `piece` enum('K','Q','R','B','N','P') DEFAULT NULL,
+  PRIMARY KEY (`x`,`y`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `board`
+--
+
+LOCK TABLES `board` WRITE;
+/*!40000 ALTER TABLE `board` DISABLE KEYS */;
+/*!40000 ALTER TABLE `board` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `game_status`
+--
+
+DROP TABLE IF EXISTS `game_status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `game_status` (
+  `status` enum('not active','initialized','started','\r\nended','aborded') NOT NULL DEFAULT 'not active',
+  `p_turn` enum('1','2') DEFAULT NULL,
+  `result` enum('B','W','D') DEFAULT NULL,
+  `last_change` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `game_status`
+--
+
+LOCK TABLES `game_status` WRITE;
+/*!40000 ALTER TABLE `game_status` DISABLE KEYS */;
+/*!40000 ALTER TABLE `game_status` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `players`
+--
+
+DROP TABLE IF EXISTS `players`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `players` (
+  `username` varchar(20) DEFAULT NULL,
+  `number_player` enum('1','2') NOT NULL,
+  PRIMARY KEY (`number_player`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `players`
+--
+
+LOCK TABLES `players` WRITE;
+/*!40000 ALTER TABLE `players` DISABLE KEYS */;
+/*!40000 ALTER TABLE `players` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `trapoula`
+--
+
+DROP TABLE IF EXISTS `trapoula`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `trapoula` (
+  `index` double DEFAULT NULL,
+  `color` char(1) DEFAULT NULL,
+  `symbol` varchar(5) DEFAULT NULL,
+  `number` int(11) DEFAULT NULL,
+  `number_player` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `trapoula`
+--
+
+LOCK TABLES `trapoula` WRITE;
+/*!40000 ALTER TABLE `trapoula` DISABLE KEYS */;
+/*!40000 ALTER TABLE `trapoula` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `trapoula_start`
+--
+
+DROP TABLE IF EXISTS `trapoula_start`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `trapoula_start` (
+  `index` double DEFAULT NULL,
+  `color` char(1) DEFAULT NULL,
+  `symbol` varchar(5) DEFAULT NULL,
+  `number` int(11) DEFAULT NULL,
+  `number_player` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `trapoula_start`
+--
+
+LOCK TABLES `trapoula_start` WRITE;
+/*!40000 ALTER TABLE `trapoula_start` DISABLE KEYS */;
+INSERT INTO `trapoula_start` VALUES (1,'B','B',1,NULL),(1,'R','KARA',2,NULL),(1,'B','S',3,NULL),(1,'R','K',4,NULL),(2,'B','B\r\n',5,NULL),(2,'R','K',6,NULL),(2,'B','S',7,NULL),(2,'R','KARA\r',8,NULL),(3,'B','B',9,NULL),(3,'R','K',10,NULL),(3,'B','S',11,NULL),(3,'R','KARA',12,NULL),(4,'B','B',13,NULL),(4,'R','K',14,NULL),(4,'B','S',15,NULL),(4,'R','KARA',16,NULL),(5,'B','B',17,NULL),(5,'R','K',18,NULL),(5,'B','S',19,NULL),(5,'R','KARA',20,NULL),(6,'B','B\r\n',21,NULL),(6,'R','K',22,NULL),(6,'B','S',23,NULL),(6,'R','KARA',24,NULL),(7,'B','B\r\n',25,NULL),(7,'R','K',26,NULL),(7,'B','S',27,NULL),(7,'R','KARA',28,NULL),(8,'B','B',29,NULL),(8,'R','K',30,NULL),(8,'B','S',31,NULL),(8,'R','KARA',32,NULL),(9,'B','B',33,NULL),(9,'R','K',34,NULL),(9,'B','S',35,NULL),(9,'R','KARA',36,NULL),(10,'B','B',37,NULL),(10,'R','K',38,NULL),(10,'B','S',39,NULL),(10,'R','KARA',40,NULL),(11,'B','B',41,NULL);
+/*!40000 ALTER TABLE `trapoula_start` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'mountzouris'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `clean_trapoula` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_ZERO_IN_DATE,NO_ZERO_DATE,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `clean_trapoula`()
+BEGIN
+	REPLACE INTO trapoula SELECT * FROM trapoula_start ;
+	UPDATE players SET username=NULL ;
+	UPDATE game_status SET  STATUS = 'not active' , p_turn=NULL , result=NULL;
+	
+	END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `delete_trapoula` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_ZERO_IN_DATE,NO_ZERO_DATE,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `delete_trapoula`()
+BEGIN
+		DELETE FROM trapoula;
+	END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `shared_trapoula` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_ZERO_IN_DATE,NO_ZERO_DATE,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `shared_trapoula`()
+BEGIN
+
+REPLACE INTO trapoula SELECT * FROM trapoula_start
+ORDER BY RAND()
+LIMIT 41;
+UPDATE trapoula SET number_player=1 ORDER BY RAND()
+LIMIT 20;
+
+
+
+
+
+
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-12-23 13:04:01
